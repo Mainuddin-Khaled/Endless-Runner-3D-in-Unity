0adb1d0dee691b24c4b75ff185cdf9649f9a733d
Mainuddin Khaled <mainuddinshahar1393@gmail.com>
2026-04-25 15:24:04 +0600

    High Score

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -5,17 +5,36 @@ public class ScoreManager : MonoBehaviour
 {
     public Transform player;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI highScoreText;
 
     private float score;
+    private int highScore;
+
+    void Start()
+    {
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
+        highScoreText.text = "Best: " + highScore;
+    }
 
     void Update()
     {
         UpdateScore();
+        CheckHighScore();
     }
 
     void UpdateScore()
     {
         score = player.position.z;
         scoreText.text = "Score: " + Mathf.FloorToInt(score);
+    }
+
+    void CheckHighScore()
+    {
+        if (score > highScore)
+        {
+            highScore = Mathf.FloorToInt(score);
+            PlayerPrefs.SetInt("HighScore", highScore);
+            highScoreText.text = "Best: " + highScore;
+        }
     }
 }
